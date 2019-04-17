2a2afd2e349658d0f8e514f2cf9ffa74ecb635ae
dante <dantinr@gmail.com>
2019-04-17 10:56:23 +0800

    微信支付

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,10 @@ Route::get('/weixin/token','Weixin\WxController@getAccessToken');       //获取
 
 Route::get('/weixin/send','Weixin\WxController@send');       //消息群发
 
+
+//微信支付
+Route::get('/weixin/pay/test','Weixin\WxPayController@test');           //消息群发
+Route::get('/weixin/pay/notify','Weixin\WxPayController@notify');       //微信支付回调地址
+
+
+
